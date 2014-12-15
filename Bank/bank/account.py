6b4ad7ae14bb42f95603f84b43f235fdc4fa8b04
cristy95 <cristylorainefuerzas@gmail.com>
2014-12-15 14:05:41 +0800

    added docstrings

--- Bank/bank/account.py
+++ Bank/bank/account.py
@@ -1,4 +1,16 @@
+"DOCSTRING"
+
 class Account(object):
+    """For the account object"""
     def __init__(self, account_number, balance):
-	    self.account_number = account_number
-	    self.balance = balance
+        """Creates an account"""
+        self.account_number = account_number
+        self.balance = balance
+
+    def changebalance(self, account_number):
+        """Changes the balance of an account"""
+        pass
+
+    def something(self, account_number):
+        """SOMETHING"""
+        pass
--- Bank/bank/bank.py
+++ Bank/bank/bank.py
@@ -1,9 +1,15 @@
+"DOCSTRING"
+
 class Bank(object):
+    """Creates the object Bank"""
     def __init__(self):
+        """Initialize an empty list of accounts"""
         self.accounts = {}
 
     def add_account(self, account):
+        """Adds an account to the list"""
         self.accounts[account.account_number] = account.balance
 
     def get_account_balance(self, account_number):
+        """gets information of a certain account"""
         return self.accounts.get(account_number)
